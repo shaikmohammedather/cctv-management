9fd5aad48e5a0bf05495a55d6115676df692ed09
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-09 11:41:22 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Sidebar } from './shared/components/sidebar/sidebar';
+import { Navbar } from './shared/components/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ATHER');
