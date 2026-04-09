266077897ea4835ac394b4b257965cf74b850854
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-09 22:46:55 +0530

    Updated footer layout with nested Bootstrap columns

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './shared/components/sidebar/sidebar';
 import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Navbar],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('ATHER');
 }
+//  Sidebar
