9756c7918e18585ba865128a1735f125db252be1
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-15 08:55:16 +0530

    added the role schema and wrote register and log in user

---
+++ backend/models/user.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const user = mongoose.model("user", userSchema);
+export default user;
