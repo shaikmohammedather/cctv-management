7f37acb6f4fa5f2793dfa94f019987a5279019d9
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-15 10:19:59 +0530

    Completed authentication system (register & login with JWT)

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API running...");
-});
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
---
+++ backend/controllers/authController.js
@@ -0,0 +1,101 @@
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+
+// REGISTER
+const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = generateToken({
+      id: newUser._id,
+      role: newUser.role,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      token,
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// LOGIN
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = generateToken({
+      id: user._id,
+      role: user.role,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { registerUser, loginUser };
