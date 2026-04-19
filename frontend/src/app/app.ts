a39f735c6225e3f7e35d8f4fa95a593f9d457fc6
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-19 10:26:31 +0530

    Frontend CRUD for customer management using Angular

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 import { Footer } from './shared/components/footer/footer';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Navbar, Footer],
--- frontend/src/app/pages/customers/customers.ts
+++ frontend/src/app/pages/customers/customers.ts
@@ -1,9 +1,56 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './customers.html',
   styleUrl: './customers.css',
 })
-export class Customers {}
+export class Customers {
+  customer: any = {};
+
+  customers: any[] = [];
+
+  showForm = false;
+  editMode = false;
+  editIndex = -1;
+
+  addCustomer() {
+    this.customer = {};
+    this.showForm = true;
+    this.editMode = false;
+  }
+
+  saveCustomer() {
+    if (this.editMode) {
+      this.customers[this.editIndex] = { ...this.customer };
+    } else {
+      this.customers.push({ ...this.customer });
+    }
+    this.resetForm();
+  }
+
+  editCustomer(index: number) {
+    this.customer = { ...this.customers[index] };
+    this.editIndex = index;
+    this.editMode = true;
+    this.showForm = true;
+  }
+
+  deleteCustomer(index: number) {
+    this.customers.splice(index, 1);
+  }
+
+  cancel() {
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.customer = {};
+    this.showForm = false;
+    this.editMode = false;
+    this.editIndex = -1;
+  }
+}
