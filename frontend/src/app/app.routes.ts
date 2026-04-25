daff7a849bf2ef277cca8b648122b48fc72443b8
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-25 10:16:39 +0530

    deleted the product section

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { Landing } from './pages/landing/landing';
 import { Services } from './pages/services/services';
 import { Customers } from './pages/customers/customers';
-import { Products } from './pages/products/products';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'services', component: Services },
   { path: 'customers', component: Customers },
-  { path: 'products', component: Products },
 ];
