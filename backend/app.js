bff059bde2eb2e98c7278bf4824b241fa611a390
shaikmohammedather <shaikmohammedather5@gmail.com>
2026-04-25 10:21:42 +0530

    Removed backend files

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,21 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+
 const app = express();
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/customer", customerRoutes);
 app.use("/api/products", productRoutes);
+app.use("/uploads", express.static("uploads"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
